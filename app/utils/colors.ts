0363680256c9af65cb0f00aceab4c736d090b197
Miroslav Myrha <kadidlo.miroslav@gmail.com>
2026-01-21 18:22:58 +0100

    Simplify getMessageColor utility

--- app/utils/colors.ts
+++ app/utils/colors.ts
@@ -1,6 +1,3 @@
-export type MessageType = 'info' | 'success' | 'warning' | 'error'
-export type GovColor = 'primary' | 'success' | 'warning' | 'error'
-
-export function getMessageColor(type: MessageType | string): GovColor {
-  return type === 'info' ? 'primary' : (type as GovColor)
+export function getMessageColor(type: string): string {
+  return type === 'info' ? 'primary' : type
 }
